a4599741cab6236fad9f3e9eb2548424db171f32
AndersErikErik <anderserikerik@gmail.com>
2023-11-13 23:01:20 +0100

    a

--- node/live/app.js
+++ node/live/app.js
@@ -25,5 +25,22 @@ console.log(birthday);
 birthday = new Date(birthday.valueOf() + 86400000);
 console.log(birthday.valueOf());
 console.log(birthday);
+ 
+
+console.log(Number.EPSILON);
+
+const result = Math.abs(0.2 - 0.3 + 0.1);
+
+console.log(result == 1);
+
+let num = new Number(55);
+console.log(num == 55);
+
+let b127 = 0b01111111;
+console.log(b127 << 1);
+
+let x127 = 0x7f;
+console.log(x127 << 1);
+
 
 
